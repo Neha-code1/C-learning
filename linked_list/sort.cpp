91945f0abb787b751cbee3ac78c46592d813f5e5
Neha-code1 <2025it0196@svce.ac.in>
2026-08-12 23:47:50 +0530

    Copying list function was added

--- linked_list/sort.cpp
+++ linked_list/sort.cpp
@@ -1,65 +1,142 @@
 #include<iostream>
 using namespace std;
-struct node{
+
+struct node
+{
     int data;
     node *next;
 };
-node *head=NULL;
-void create(int n){
+
+node *head = NULL;
+
+// CREATE ORIGINAL LIST
+void create(int n)
+{
     node *temp;
     node *newnode;
-    for(int i=0;i<n;i++){
-        newnode=new node;
-        cout<<"Enter value to be inserted\n";
-        cin>>newnode->data;
-        newnode->next=NULL;
-        if(head==NULL){
-            head=newnode;
-            temp=newnode;
+
+    for(int i = 0; i < n; i++)
+    {
+        newnode = new node;
+
+        cout << "Enter value to be inserted: ";
+        cin >> newnode->data;
+
+        newnode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = newnode;
+            temp = newnode;
         }
-        else{
-            temp=head;
-            while(temp->next!=NULL){
-                temp=temp->next;
+        else
+        {
+            temp = head;
+
+            while(temp->next != NULL)
+            {
+                temp = temp->next;
             }
-            temp->next=newnode;
+
+            temp->next = newnode;
         }
     }
 }
-void display(int n){
-    node *temp;
-    temp=head;
-    cout<<"Values:\n";
-    while(temp!=NULL){
-        cout<<temp->data<<"->";
-        temp=temp->next;
+
+// DISPLAY LIST
+void display(node *head)
+{
+    node *temp = head;
+
+    cout << "Values:\n";
+
+    while(temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
     }
-    cout<<"\nAddress:\n";
-    temp=head;
-    while(temp!=NULL){
-        cout<<temp<<"->";
-        temp=temp->next;
-    }
+
+    cout << "NULL\n";
 }
-void sortlist(){
+
+// COPY ORIGINAL LIST
+node* copylist()
+{
+    node *temp = head;
+
+    node *copyhead = NULL;
+    node *copytemp = NULL;
+
+    while(temp != NULL)
+    {
+        node *newnode = new node;
+
+        newnode->data = temp->data;
+        newnode->next = NULL;
+
+        if(copyhead == NULL)
+        {
+            copyhead = newnode;
+            copytemp = newnode;
+        }
+        else
+        {
+            copytemp->next = newnode;
+            copytemp = newnode;
+        }
+
+        temp = temp->next;
+    }
+
+    return copyhead;
+}
+
+// SORT THE COPIED LIST
+void sortlist(node *copyhead)
+{
     int temp;
+
     node *i;
     node *j;
-    for(i=head;i!=NULL;i=i->next){
-        for(j=i->next;j!=NULL;j=j->next){
-            if(i->data>j->data){
-                temp=i->data;
-                i->data=j->data;
-                j->data=temp;
+
+    for(i = copyhead; i != NULL; i = i->next)
+    {
+        for(j = i->next; j != NULL; j = j->next)
+        {
+            if(i->data > j->data)
+            {
+                temp = i->data;
+                i->data = j->data;
+                j->data = temp;
             }
         }
     }
 }
-int main(){
-    create(4);
-    display(4);
-    sortlist();
-    cout << "\nSorted list:\n";
-    display(4);
+
+int main()
+{
+    int n;
+
+    cout << "Enter number of nodes: ";
+    cin >> n;
+
+    // Create L
+    create(n);
+
+    cout << "\nOriginal List L:\n";
+    display(head);
+
+    // Create L'
+    node *copyhead = copylist();
+
+    cout << "\nCopied List L':\n";
+    display(copyhead);
+
+    // Sort L'
+    sortlist(copyhead);
+
+    cout << "\nSorted List L':\n";
+    display(copyhead);
+
     return 0;
 }
